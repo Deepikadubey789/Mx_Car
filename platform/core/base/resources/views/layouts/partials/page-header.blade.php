7ab94f3ff48053cf80ac40ac9d0a60cc1164184d
mangi.lal <mangi.lal@miracuves.com>
2026-04-02 17:53:28 +0530

    Refactor page header to use custom admin styling for improved layout

--- platform/core/base/resources/views/layouts/partials/page-header.blade.php
+++ platform/core/base/resources/views/layouts/partials/page-header.blade.php
@@ -1,4 +1,4 @@
-<div class="page-header d-print-none">
+<div class="admin-page-header d-print-none">
     <div class="{{ AdminAppearance::getContainerWidth() }}">
         <div class="row g-2 align-items-center">
             <div class="col">
